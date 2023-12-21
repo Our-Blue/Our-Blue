76060930a413e63304558bf0cc8b44b7de09d6ba
EC2 Default User <ec2-user@ip-10-0-1-254.ap-northeast-1.compute.internal>
2023-12-21 16:47:44 +0000

    initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,8 @@
                         </g>
                     </svg>
                 </div>
+                
+                                <p><a href="{{ route('hello') }}">Hello World</a></p
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
