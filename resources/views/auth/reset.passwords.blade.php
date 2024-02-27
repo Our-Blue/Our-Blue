4b98b85337b0357884139c4e1041d7cdc1064c09
EC2 Default User <ec2-user@ip-10-0-1-146.ap-northeast-1.compute.internal>
2024-02-27 23:37:41 +0000

    Auth処理変更_2024/02/28

--- resources/views/auth/reset.passwords.blade.php
+++ resources/views/auth/reset.passwords.blade.php
@@ -2,21 +2,46 @@
 
 @section('content')
     <div class="container">
-        <h2>パスワードリセット</h2>
-        @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
-        <form method="POST" action="{{ route('password.email') }}">
+        <h2>新規ユーザー登録</h2>
+        <form method="POST" action="{{ route('register') }}">
             @csrf
 
             <div class="form-group">
-                <label for="email">メールアドレス</label>
-                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                <label for="name">名前</label>
+                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
 
-            <button type="submit" class="btn btn-primary">パスワードリセットリンクを送信</button>
+            <div class="form-group">
+                <label for="email">メールアドレス</label>
+                <input id="email" type="email" class="form-control @error('mail') is-invalid @enderror" name="mail" value="{{ old('mail') }}" required autocomplete="email">
+                @error('mail')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="password">パスワード</label>
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="password-confirm">パスワード確認</label>
+                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+            </div>
+
+            <button type="submit" class="btn btn-primary">登録</button>
         </form>
     </div>
 @endsection
