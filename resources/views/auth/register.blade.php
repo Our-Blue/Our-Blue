1eca4b86cca7520dd352084ce376e48594b8ab7b
EC2 Default User <ec2-user@ip-10-0-1-146.ap-northeast-1.compute.internal>
2024-02-28 18:49:25 +0000

    Auth処理変更_2024/02/28

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,31 +3,43 @@
 @section('content')
     <div class="container">
         <h2>新規ユーザー登録</h2>
-        <form method="POST" action="{{ route('users.register') }}">
+        <form method="POST" action="{{ route('register') }}">
             @csrf
 
             <div class="form-group">
                 <label for="name">名前</label>
-                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
 
             <div class="form-group">
                 <label for="email">メールアドレス</label>
-                <input id="email" type="email" class="form-control" name="mail" value="{{ old('mail') }}" required autocomplete="email">
+                <input id="email" type="email" class="form-control @error('mail') is-invalid @enderror" name="mail" value="{{ old('mail') }}" required autocomplete="email">
+                @error('mail')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
 
             <div class="form-group">
                 <label for="password">パスワード</label>
-                <input id="password" type="password" class="form-control" name="password" required autocomplete="new-password">
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
 
             <div class="form-group">
                 <label for="password-confirm">パスワード確認</label>
                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
             </div>
-            
-            <!-- hiddenフィールドでroleを0で送信 -->
-            <input type="hidden" name="role" value="0">
 
             <button type="submit" class="btn btn-primary">登録</button>
         </form>
