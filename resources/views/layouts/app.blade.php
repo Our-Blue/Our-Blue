bbdd8382e22b31ea4c53c598bd1fbb572767d2f7
EC2 Default User <ec2-user@ip-10-0-1-254.ap-northeast-1.compute.internal>
2024-03-04 00:44:52 +0000

    ルートとヘッダー修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,11 @@
        @if(Auth::check())
         @endif　　　　　　　<!--ログイン認証完成後、検索とマイページをif文の中に移動*/-->
         <div class="search_div">
+<<<<<<<<< saved version
             <form action=""{{ route('result') }}"" method="GET" class="search_from"><!--form action""のところに遷移先のroute-->
+=========
+            <form action=""{{ route('result') }}"" method="GET" class="search_from">
+>>>>>>>>> local version
                 <input type="search" placeholder="フリーワード検索" name="search" value="@if (isset($search)) {{ $search }} @endif">
                 <input type="submit" value="検索" class="search_button">
             </form>
