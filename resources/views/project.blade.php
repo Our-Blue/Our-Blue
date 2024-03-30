5db1e84435e832ac78266d5c0c5d9069b58e27da
EC2 Default User <ec2-user@ip-10-0-1-146.ap-northeast-1.compute.internal>
2024-03-30 09:52:41 +0000

    2024/03/30 改修2

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+@section('content')
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -77,3 +80,4 @@
 
     </body>
 </html>
+@endsection
