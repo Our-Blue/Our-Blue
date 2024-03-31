b3869e25564da8b8881716bd15b231790e5cc463
EC2 Default User <ec2-user@ip-172-31-46-111.ap-northeast-1.compute.internal>
2024-03-31 05:04:38 +0000

    14:04コンフリクト解消

--- routes/web.php
+++ routes/web.php
@@ -19,42 +19,6 @@ use App\Http\Controllers\Auth\ResetPasswordController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-<<<<<<< HEAD
-
-// ログイン関連のルート
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login'); // ログイン画面の表示
-Route::post('/login/', [LoginController::class, 'login']); // ログイン処理
-
-Route::get('/password/request', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
-Route::post('/password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-Route::get('/password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('/password/update', [ResetPasswordController::class, 'reset'])->name('password.update');
-
-// ユーザー新規登録関連のルート
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register'); // ユーザー新規登録画面へのルート
-Route::post('/register/store', [RegisterController::class, 'register'])->name('users.register'); // ユーザー登録処理へのルート // ユーザー登録処理へのルート
-
-// プロジェクト関連のルート
-Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
-Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
-Route::get('/projects/{project}/confirm', [ProjectController::class, 'confirm'])->name('projects.confirm');
-Route::post('/projects/store/', [ProjectController::class, 'store'])->name('projects.store');
-Route::put('/projects/{project}/update', [ProjectController::class, 'update'])->name('projects.update');
-Route::get('/project/{id}',[ProjectController::class, 'show'])->name('project');
-
-
-// チケット関連のルート
-Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
-Route::post('/tickets/', [TicketController::class, 'store'])->name('tickets.store');
-Route::get('/tickets/{id}', [TicketController::class, 'show'])->name('tickets.show');
-Route::get('/tickets/{id}/edit', [TicketController::class, 'edit'])->name('tickets.edit');
-Route::put('/tickets/{id}/edit', [TicketController::class, 'update'])->name('tickets.update');
-
-Route::resource('mypage', MypageController::class);
-Route::get('/password', [MypageController::class,'ChangePasswordForm'])->name('password_form');
-
-Route::get('/result/{projectId}/{ticketId}', [SearchController::class, 'result'])->name('result');
-=======
 Route::group(['middleware' => 'auth'], function () {
     // ログイン関連のルート
     Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login'); // ログイン画面の表示
@@ -92,4 +56,3 @@ Route::group(['middleware' => 'auth'], function () {
     
     Route::get('/', [TopController::class, 'index']);
  });
->>>>>>> 3ed29d170092fe8e75f2d05c08dc4e82d2bd8119
